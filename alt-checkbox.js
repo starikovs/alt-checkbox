cf43d6cf11485fcfaa678f2d853ec990f5ac8f95
starikovs <slavicstar@gmail.com>
2013-02-07 22:42:28 +0200

    data-alt-class instead of data-icon and data-size, data-alt-class contains classes for alt-checkbox-dress

--- alt-checkbox.js
+++ alt-checkbox.js
@@ -1,20 +1,18 @@
 jQuery(function($) {
     $(".alt-checkbox").hide().each(function() {
         var cb = $(this),
-            cbIcon = cb.attr("data-icon"),
-            cbSize = cb.attr("data-size"),
-            alt = $("<a href=\"#\" class=\"alt-checkbox-dress\">").addClass(cbSize).insertBefore(cb);
+            cbAltClass = cb.attr("data-alt-class"),
+            alt = $("<a href=\"#\" class=\"alt-checkbox-dress\">").addClass(cbAltClass).insertBefore(cb);
 
         alt.click(function(event) {
             var alt = $(this);
             if (alt.hasClass("checked")) {
-                alt.removeClass("checked " + cbIcon);
+                alt.removeClass("checked");
                 cb.prop("checked", false);
             } else {
-                alt.addClass("checked " + cbIcon);
+                alt.addClass("checked");
                 cb.prop("checked", true);
             }
-            //alert(checkbox.is(":checked"));
             event.preventDefault();
         }).bind("keyup", function(event) {
             if (event.keyCode === 32) {
@@ -28,33 +26,4 @@ jQuery(function($) {
 
         alt.addClass(cb.is(":checked") ? "checked" : "");
     });
-
-    /*var cb = $(".alt-checkbox").hide(),
-        alt = $("<a href=\"#\" class=\"alt-checkbox-dress\">").insertBefore(cb);
-
-*/
-/*
-    alt.click(function(event) {
-        var alt = $(this);
-        if (alt.hasClass("checked")) {
-            alt.removeClass("checked " + cbIcon);
-            cb.prop("checked", false);
-        } else {
-            alt.addClass("checked " + cbIcon);
-            cb.prop("checked", true);
-        }
-        //alert(checkbox.is(":checked"));
-        event.preventDefault();
-    }).bind("keyup", function(event) {
-            if (event.keyCode === 32) {
-                $(this).click();
-            }
-        });
-
-    cb.bind("change", function() {
-        alt.click();
-    });
-
-    alt.addClass(cb.is(":checked") ? "checked" : "");
-    */
 });
