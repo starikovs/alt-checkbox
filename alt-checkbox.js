46c38bf02609bda1dbb5f3bc2349e5ffb5a4673a
starikovs <slavicstar@gmail.com>
2013-02-08 23:56:20 +0200

    Move to jQuery plugin.

--- alt-checkbox.js
+++ alt-checkbox.js
@@ -1,29 +1,57 @@
-jQuery(function($) {
-    $(".alt-checkbox").hide().each(function() {
-        var cb = $(this),
-            cbAltClass = cb.attr("data-alt-class"),
-            alt = $("<a href=\"#\" class=\"alt-checkbox-dress\">").addClass(cbAltClass).insertBefore(cb);
+(function($) {
+    var methods = {
+        init: function () {
+            return this.hide().each(function() {
+                var cb = $(this);
+                if (cb.prev(".alt-checkbox-dress").length > 0) {
+                    return;
+                }
 
-        alt.click(function(event) {
-            var alt = $(this);
-            if (alt.hasClass("checked")) {
-                alt.removeClass("checked");
-                cb.prop("checked", false);
-            } else {
-                alt.addClass("checked");
-                cb.prop("checked", true);
-            }
-            event.preventDefault();
-        }).bind("keyup", function(event) {
-            if (event.keyCode === 32) {
-                $(this).click();
-            }
-        });
+                var cbAltClass = cb.attr("data-alt-class"),
+                    alt = $("<a href=\"#\" class=\"alt-checkbox-dress\">").addClass(cbAltClass).insertBefore(cb);
 
-        cb.bind("change", function() {
-            alt.click();
-        });
+                alt.bind("click.alt-checkbox", function(event) {
+                    var alt = $(this);
+                    if (alt.hasClass("checked")) {
+                        alt.removeClass("checked");
+                        cb.prop("checked", false);
+                    } else {
+                        alt.addClass("checked");
+                        cb.prop("checked", true);
+                    }
+                    event.preventDefault();
+                }).bind("keyup.alt-checkbox", function(event) {
+                    if (event.keyCode === 32) {
+                        $(this).click();
+                    }
+                });
 
-        alt.addClass(cb.is(":checked") ? "checked" : "");
-    });
-});
+                cb.bind("change.alt-checkbox", function() {
+                    alt.click();
+                });
+
+                alt.addClass(cb.is(":checked") ? "checked" : "");
+            });
+        },
+        clear: function() {
+            return this.each(function() {
+                var cb = $(this),
+                    alt = cb.prev(".alt-checkbox-dress");
+
+                alt.unbind(".alt-checkbox").remove();
+                cb.unbind(".alt-checkbox");
+            }).show();
+        }
+    };
+
+    $.fn.altCheckbox = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === "object" || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error("Method " +  method + " does not exist on jQuery.altCheckbox.");
+        }
+    };
+
+})(jQuery);
